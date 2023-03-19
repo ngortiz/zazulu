709eac6e0d9ab5f2c2bbd58fcec40fa69f9bde82
ngortiz <nidigortiz21@gmail.com>
2023-03-19 12:48:09 -0300

    Formulario de animales

--- src/App.js
+++ src/App.js
@@ -1,53 +1,102 @@
-import './App.css';
+import React from 'react';
+import { useState } from 'react';
+import { Container, TextField, Button } from '@mui/material';
 import InformacionMascota from './componentes/InformacionMascota';
+import './App.css'
 
 const animales = [
   {
     imagen: 'perro01.jpg',
-    nombre: 'pipi',
-    sexo: 'macho',
+    nombre: 'Pepe',
+    sexo: 'Macho',
     ciudad: 'Encarnacion',
-    descripcion: 'de raza'
+    descripcion: 'De raza'
   },
   {
     imagen: 'perro02.jpg',
-    nombre: 'travolta',
-    sexo: 'macho',
+    nombre: 'Travolta',
+    sexo: 'Macho',
     ciudad: 'Encarnacion',
-    descripcion: 'de raza'
+    descripcion: 'De raza'
   },
   {
     imagen: 'gatito01.jpg',
-    nombre: 'zari',
-    sexo: 'hembra',
+    nombre: 'Zari',
+    sexo: 'Hembra',
     ciudad: 'Encarnacion',
-    descripcion: 'raza comun'
+    descripcion: 'De raza'
   },
   {
     imagen: 'gatito02.jpg',
-    nombre: 'pepito',
-    sexo: 'macho',
+    nombre: 'Pepito',
+    sexo: 'Macho',
     ciudad: 'Encarnacion',
-    descripcion: 'raza comun'
+    descripcion: 'De raza'
   }
 
 ]
 function App() {
-  return (
-    <div className="contenedor">
-      {animales.map((animal, indice)=>{
-       return <InformacionMascota 
-        imagen={animal.imagen}
-        nombre={animal.nombre}
-        sexo={animal.sexo}
-        ciudad={animal.ciudad}
-        descripcion={animal.descripcion}
-        key={indice}
-        />
-      })}
-      
+  const [nombre, setNombre] = useState('')
+  const [sexo, setSexo] = useState('')
+  const [ciudad, setCiudad] = useState('')
+  const [descripcion, setDescripcion] = useState('')
+  const [listaAnimales, setListaAnimales] = useState(animales)
 
-    </div>
+  const registrar = (e) => {
+    const nuevoAnimal ={
+      imagen: 'gatito02.jpg',
+      nombre: nombre,
+      sexo: sexo,
+      ciudad: ciudad,
+      descripcion: descripcion,
+    }
+    const nuevosAnimales = [nuevoAnimal,...listaAnimales];
+    setListaAnimales(nuevosAnimales)
+  }
+
+  const setearNombre = (e) => {
+    setNombre(e.target.value);
+  }
+
+  const setearSexo= (e) => {
+    setSexo(e.target.value);
+  }
+  const setearDescripcion= (e) => {
+    setDescripcion(e.target.value);
+  }
+
+  const setearCiudad= (e) => {
+    setCiudad(e.target.value);
+  }
+
+  return (
+    <>
+      <img alt='banner' src={require(`./imagenes/banner.jpg`)} className="informacion-mascota-banner" />
+
+      <Container fixed className="formulario">
+        <form>
+          <TextField id="nombre" label="Nombre" variant="outlined" onChange={setearNombre}/>
+          <TextField id="sexo" label="Sexo" variant="outlined"  onChange={setearSexo}/>
+          <TextField id="descripcion" label="Descripcion" variant="outlined" onChange={setearDescripcion}/>
+          <TextField id="ciudad" label="Ciudad" variant="outlined" onChange={setearCiudad}/>
+          <Button variant="contained" className='boton-registrar' onClick={registrar}>Agregar</Button>
+        </form>
+      </Container>
+
+      <Container fixed className="informacion">
+        {listaAnimales.map((animal, indice) => {
+          return <InformacionMascota
+            imagen={animal.imagen}
+            nombre={animal.nombre}
+            sexo={animal.sexo}
+            ciudad={animal.ciudad}
+            descripcion={animal.descripcion}
+            key={indice}
+          />
+        })}
+      </Container>
+
+    </>
   );
 }
 
--- src/componentes/InformacionMascota.js
+++ src/componentes/InformacionMascota.js
@@ -1,13 +1,31 @@
 import React from "react";
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
 
-const InformacionMascota = ({imagen,  nombre, sexo, ciudad, descripcion}) => (
-    <div className="informacion-mascota">
-        <img src={require(`../imagenes/${imagen}`)}/>
-        <p>Nombre:{nombre}</p>
-        <p>Sexo:{sexo}</p>
-        <p>Ciudad:{ciudad}</p>
-        <p>Descripcion:{descripcion}</p>
-    </div>
+const InformacionMascota = ({ imagen, nombre, sexo, ciudad, descripcion }) => (
+    <Card sx={{ maxWidth: 345 }} className="informacion-mascota">
+      <CardMedia
+        sx={{ height: 140 }}
+        image={require(`../imagenes/${imagen}`)}
+        title={nombre}
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          {nombre}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          {descripcion}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Adpotar</Button>
+        <Button size="small">Leer mas</Button>
+      </CardActions>
+    </Card>
 )
 
 export default InformacionMascota;
